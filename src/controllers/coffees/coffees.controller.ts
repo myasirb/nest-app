ad534855a26fe5ed7661763be8c8357c97f5ff5b
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-06 14:59:28 +0500

    Controller added

--- src/controllers/coffees/coffees.controller.ts
+++ src/controllers/coffees/coffees.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   getAll() {
     return 'This returns all coffees';
+  }
+
+  @Get(':id') // Dynamic Route Registered
+  getById(@Param('id') id: String) {
+    // Controller Function with params as parameter and exacting id from it to id variable with its type
+    return `This Returns coffee with id : ${id}`;
+  }
+
+  @Post()
+  postCoffee(@Body() body) {
+    const { name } = body;
+    return name;
   }
 }
