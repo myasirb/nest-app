5fe1a720d975e56d491d49c9abd4bbd59b8125bd
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-07 13:36:47 +0500

    Validation Flags implemented

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    // This helps transform the type of request payload to the type we have set
     new ValidationPipe({
-      transform: true,
+      whitelist: true, // If any field is not in DTO and is passed, do remove that filed and process payload
+      forbidNonWhitelisted: true, // If any field is passed which is not in DTO, simply do not allow that request to be processed
+      transform: true, // This helps transform the type of request payload to the type we have set
     }),
   );
   await app.listen(5000);
