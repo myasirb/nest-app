e0fcc5fd77f328194a6d367a4116f4164a00e332
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-07 11:50:28 +0500

    transform flag

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    // This helps transform the type of request payload to the type we have set
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
