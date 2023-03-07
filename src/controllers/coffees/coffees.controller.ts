e9a8593225d872975644e05788b5acc6b36cb17c
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-07 10:41:37 +0500

    Pagination added

--- src/controllers/coffees/coffees.controller.ts
+++ src/controllers/coffees/coffees.controller.ts
@@ -7,24 +7,31 @@ import {
   Res,
   Patch,
   Delete,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  getAll() {
-    return 'This returns all coffees';
+  getByLimit(@Query() paginationParam) {
+    const { limit, offset } = paginationParam;
+    return `This Has Limit : ${limit} and offset : ${offset}`;
   }
 
-  @Get('/res')
-  getByRes(@Res() res) {
-    res.status(200).json({ name: 'Hola' });
+  @Get()
+  getAll() {
+    return 'This returns all coffees';
   }
 
   @Get(':id') // Dynamic Route Registered
   getById(@Param('id') id: String) {
     // Controller Function with params as parameter and exacting id from it to id variable with its type
     return `This Returns coffee with id : ${id}`;
+  }
+
+  @Get('/res')
+  getByRes(@Res() res) {
+    res.status(200).json({ name: 'Hola' });
   }
 
   @Post()
