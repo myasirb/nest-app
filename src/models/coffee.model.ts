b9109c3b575e43a026a052693437450df5fbc566
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-07 14:42:34 +0500

    Added decorates to make columns

--- src/models/coffee.model.ts
+++ src/models/coffee.model.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // Table name would be coffee
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
 }
