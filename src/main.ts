e03bf1fee1efe43a942fb3eda7dcb262a668e3af
Muhammad Yasir <muhammadyasir@folio3.com>
2023-03-08 12:17:03 +0500

    implicit conversion set to true

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // If any field is not in DTO and is passed, do remove that filed and process payload
       forbidNonWhitelisted: true, // If any field is passed which is not in DTO, simply do not allow that request to be processed
-      transform: true, // This helps transform the type of request payload to the type we have set
+      transform: true, // This helps transform the type of request payload to the type we have set4
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(5000);
